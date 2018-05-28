a3193ca0dd950c80503aaf1742ab27350dfa795f
Howie <zhaohuayu1996@gmail.com>
2018-05-28 23:44:27 +0800

    modify request method for login to POST

--- src/main/java/com/ven/controller/LoginController.java
+++ src/main/java/com/ven/controller/LoginController.java
@@ -5,10 +5,13 @@ import com.hiber.session.LoginSession;
 import com.ven.service.LoginSelector;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
+
 @RequestMapping("/user")
 @Controller
 public class LoginController {
@@ -25,10 +28,10 @@ public class LoginController {
         return loginSelector.allUser();
     }
 
-    @RequestMapping("/login")
+    @RequestMapping(value="/login",method = RequestMethod.POST)
     @ResponseBody
-    public String login(@RequestParam(value="username",required = true)String username,
-                        @RequestParam(value="password",required = true) String password)
+    public Map<String,String> login(@RequestParam(value="username",required = false)String username,
+                                    @RequestParam(value="password",required = false) String password)
     {
         return loginSelector.check(username,password);
     }
--- src/main/java/com/ven/service/LoginSelector.java
+++ src/main/java/com/ven/service/LoginSelector.java
@@ -4,20 +4,34 @@ import com.hiber.model.LoginEntity;
 import com.hiber.session.LoginSession;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service()
 public class LoginSelector {
-    public String check(String username, String password) {
+    public Map<String,String> check(String username, String password) {
+        Map<String,String> res=new HashMap<>();
+        res.put("Error","");
+        res.put("Identity","");
         LoginSession session = new LoginSession();
-        LoginEntity res = session.select_by_username(username);
-        if (res == null)
-            return "No such user";
+        LoginEntity res_entity = session.select_by_username(username);
+        if (res_entity == null)
+        {
+            res.put("Error","No such user");
+            return res;
+        }
         else {
-            if (!res.getuPassword().equals(password)) {
-                return "Password Error";
+            if (!res_entity.getuPassword().equals(password)) {
+                {
+                    res.put("Error","Password Error");
+                    return res;
+                }
             } else
-                return res.getIdentity().toString();
+            {
+                res.put("Identity",res_entity.getIdentity().toString());
+                return res;
+            }
         }
     }
 
